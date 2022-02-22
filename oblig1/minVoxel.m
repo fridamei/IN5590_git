b2fe702e52f4c0bfb6dcc00ba20d0e7b52ccaf5a
fridamei <fridamei@ifi.uio.no>
2022-02-22 17:12:47 +0100

    Første oblig, fungerer greit, men mangler de andre filene

---
+++ oblig1/minVoxel.m
@@ -0,0 +1,93 @@
+%{ 
+fid = fopen('filNavn.gcode', 'w', 'n','UTF-8');
+% Ren tekst ut   
+fprintf(fid, '%s\n', '# !File ; generated at 22.09.2021 - 21:31');
+% Lager en string som inneholder verdien til en variabel omsluttet av tekst
+val = 22.3;
+stringUt = ['noe tekst ' num2str(val) 'enda mer tekst'];
+fprintf(fid, '%s\n', stringUt);
+fclose(fid);
+%}
+
+
+startPoint = "G0 F6000 X107.5 Y107.5 Z0.2";
+
+% Box dimensions
+boxHeight = 10; % mm
+boxBreadth = 20; % mm, defined by number of lines
+boxLength = 20; % mm, equals the length of every printed line
+% dimension of box is then boxBreadth x boxLength x boxHeigth
+
+
+% Print dimensions (find optimal values by testing)
+layerHeight = 0.3; % mm Defines the size to increment z-axis with https://the3dprinterbee.com/3d-printing-layer-height-vs-nozzle-size/ 
+lineWidth = 0.2; % mm. The distance between two lines in the same layer
+eRate = 0.033; % extrusion per mm 
+
+
+% Start coordinates
+startX = 107.5;
+startY = 107.5;
+startZ = layerHeight;
+z = startZ;
+
+% Extrusion Values
+eLine = eRate*boxLength; % Amount to extrude per line
+eTotal = 0; % Cumulated extruted filament
+
+% Number of lines and layers
+nLayers = round(boxHeight/layerHeight); % Number of layers given the set box height and layer height
+nLines = round(boxBreadth/lineWidth); % The 
+
+% Defines direction on x-axis 
+toRight = 1; % Defines if the line is printed from left to right or vice versa
+
+
+
+fid = fopen('cube.gcode', 'w', 'n','UTF-8');
+g0_start = ['G0 F6000 X' num2str(startX) ' Y' num2str(startY) ' Z' num2str(startZ)];
+fprintf(fid, '%s\n');
+
+% For every layer
+for i = 1:nLayers
+    
+    %tkst = [';LAYER:' num2str(i) '\n;TYPE:SKIN']
+    %g0_layer = ['G0 F6000 Z' num2str(z)];
+    
+    g0_layer = [';LAYER:' num2str(i) newline ';TYPE:SKIN' newline 'G0 F6000 Z' num2str(z)];
+    fprintf(fid, '%s\n', g0_layer);    
+    
+    %toRight = 1; Hvis x-verdien skal resettes, må denne det også
+    %x = startX;
+    y = startY;
+    
+ 
+   % For every line
+    for j = 1:nLines
+        
+        % G1-kommando (ekstruderer bare når dysen beveger seg i x-retning):
+        x = x + boxLength * toRight;
+        
+        eTotal = eTotal + eLine;
+        
+        g1_out = ['G1 F600 X' num2str(x) ' E' num2str(eTotal)];
+        
+        
+        % G0-kommando:
+        y = y + lineWidth;
+        
+        g0_out = ['G0 F6000 Y' num2str(y)];
+        
+        
+        fprintf(fid, '%s\n', g1_out, g0_out);
+        
+        toRight = toRight*-1; % Invert direction of x-axis movement
+    
+    end
+    
+  
+    % Increment z to make next layer
+    z = z + layerHeight;
+end
+
+fclose(fid);
